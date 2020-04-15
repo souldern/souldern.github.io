25f391dc9efc036661bf63a1aeb57a774d6fbd89
David Carlisle <d.p.carlisle@gmail.com>
2020-04-15 21:32:35 +0100

    updates

---
+++ home/quiz/quiz.js
@@ -0,0 +1,60 @@
+
+var thisQ=1;
+var answers=[];
+
+function squiz () {
+    var q=document.getElementById("q");
+    var fdiv=document.getElementById("fdiv");
+    var im=document.getElementById("im");
+    if(thisQ > questions.length ) {
+	document.getElementById("next").style.display="none";
+	im.style.display="none";
+	q.innerHTML="Results";
+	var total=0;
+	for (var i=0;i< questions.length; i++) {
+	    var l = document.createElement("div");
+	    if(answers[i] == questions[i][3]) {
+		l.style.backgroundColor="#AFA";
+		total+=1;
+	    } else {
+		l.style.backgroundColor="#FAA";
+	    }
+	    l.innerHTML=i+1 + ": " + questions[i][2][answers[i]];
+	    fdiv.appendChild(l);
+	}
+	var l = document.createElement("div");
+	l.innerHTML="<b>Your total: " + total + " / " + questions.length + "</b>" ;
+	fdiv.parentNode.appendChild(l);
+       if(total == questions.length)  {
+       document.getElementById("win").style.display="block";
+}
+
+    } else {
+	q.innerHTML="Question " + thisQ + "/" + questions.length + ": " + questions[thisQ - 1][0];
+	if(questions[thisQ - 1][1] == "") {
+	    im.style.display="none";
+	} else {
+	    im.style.display="block";
+	    im.setAttribute("src",questions[thisQ - 1][1]);
+	}
+    for (var i=0;i< questions[thisQ - 1][2].length; i++) {
+	var l = document.createElement("div");
+	l.innerHTML="<label><input type=\"radio\" name=\"ans\" class=\"a\" value=\"" + i + "\">" + questions[thisQ - 1][2][i] + "</label>";
+	fdiv.appendChild(l);
+    }
+    }
+}
+
+function nextpage() {
+    var radios = document.querySelectorAll('input[type="radio"]:checked');
+    if (radios.length>0) {
+	answers[thisQ-1]=radios[0].value;
+    thisQ+=1;
+    document.getElementById("fdiv").innerHTML="";
+	squiz();
+    } else {
+	alert("Please select an answer");
+    }
+}
+
+window.addEventListener('load', squiz, false);
---
+++ home/quiz/test.js
@@ -0,0 +1,7 @@
+
+var questions = [
+    [ "what is 1+1?","../houses/dovetail.jpg",["2 yes","3","4"],0],
+    [ "village is this", "../houses/willowhouse.jpg",["Fritwell","Ardley","Souldern yes"],2],
+    [ "who is this","",["me","you yes"],1]
+];
+
