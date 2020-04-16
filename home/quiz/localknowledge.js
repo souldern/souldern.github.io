2888d6fee86eda19561f832e1eb31edbd61cd92e
David Carlisle <d.p.carlisle@gmail.com>
2020-04-16 23:16:38 +0100

    updates

--- home/quiz/localknowledge.js
+++ home/quiz/localknowledge.js
@@ -1,8 +1,8 @@
 
 var questions = [
     [ "An old name for Souldern was:","https://lh3.googleusercontent.com/lbtW_1cP41oYkMt4qAOwVbr-FwlbBSvEGRP3kzeVXYG-JgfSbHEGmDYGIHbAbzm4Am5vFt9Dn-CJ44hAcsITaRC2ETJfnBHayilcIB5ZDwbZEMFrAM8U31ZWosfsFyK4VSdh7zB56xGS5969zeTE-P5GLZjQ7KnPzGcqLpN_n6TadJQaeOzTl8X8hCVHdNSCy-6W1iGCCFiYuzpWHdjYSul7cezDwFZYYhCo9IFzeh_sTKa8Pp5zDRSY7q3HSgf__1cn0YWY09iVLTQBjBBcGZQ-HruCH0lYVIdYMpgetl4KY_JxYfMhpTMLhXj6frhMnMbRCuWSJ7qwuwLENPQihD734pDzRUDf6S-N3KAKioNq9yxgHkea98SZLv5AR0_Bkn5rppli57reHcYidtllGj7DcN7-ZAh_s48xssPS4YzpW0QX3IrKQMLqmtR0iRCtOU6xYbno4QbQr6u2eKms2sbHsbu9JS14Of8OZ9sjh1IzX1VkxMsUgs9rA2f4C31uaGC1t8myg_si7D6PKGzNGkBJxzwsm28l6ligplYm6aIR_8czWOxnLvx5ZGBvXxXWCuJbPXohuN_bpvKVt6TqKFX2nGtDfCuMU-DzT9F90dBVUJFvj-PjDYwxD5EqrPuakk1hIVu2hOruEAgESkDrLQBNum1QnTGAedTtlxypJnKdPEMwPOrUZ-o2UiciUfGrJvpIjlanTh1T-Ts8nXubITrVjvVvV3N0wrpHGWxmcVwg5gPlmd1LZQ=w1559-h877-no",["Soldon","Sulforn","Sulthorn"],2],
-    [ "What year did the railway open over the Viaduct", "https://lh3.googleusercontent.com/-UYWuJbZUnLc/Wf78hfGLoNI/AAAAAAAAEDk/brWwJ0SY0SgAExnkfob92Q4gAZMvom--QCMQCGAYYCw/s400/20171105_111415.jpg",["1910","1880","1850"],0],
-    [ "The full name of the Parish Church is","https://www.souldern.org/parish-church/church.jpg",["St. Mary's Church, Souldern","The Church of The Annunciation of the Blessed Virgin Mary", "The Church of The Annunciation of the Virgin Mary"],1],
+    [ "What year did the railway open over the Viaduct?", "https://lh3.googleusercontent.com/-UYWuJbZUnLc/Wf78hfGLoNI/AAAAAAAAEDk/brWwJ0SY0SgAExnkfob92Q4gAZMvom--QCMQCGAYYCw/s400/20171105_111415.jpg",["1910","1880","1850"],0],
+    [ "The full name of the Parish Church is:","https://www.souldern.org/parish-church/church.jpg",["St. Mary's Church, Souldern","The Church of The Annunciation of the Blessed Virgin Mary", "The Church of The Annunciation of the Virgin Mary"],1],
     ["What Role does he have","https://lh3.googleusercontent.com/TPODTL0pHJjW8LgNkxyq2BVjr0lmZIAzpRrxPt8MG-TJ99laz9XgSx-UQuiUpqdi7rprqVName8d_UXpwEjnm94PDm9jIPC1A9QCS0ejwB9zFBn1ypThzSC5yASakOkRgC48oCp83kvQXN4biA21hIrM74VwfkQ8exDId31ZwdirIAQDOEgPkSq802msh0qVNPSx1HAio3HBnuNhpBIQNNZVR6ufWAHOqH0pe7ZbQAa1TYMweCeJlnJ420aTULsOouTpW8t3_sSKuuc0FLlvQO-QELDf0ZMnh2IuLwyhSIrPCPuNC5GrFotkD_G_SL8tKhxMWliI55hwtBK4b3yChcZ65BdUC5ufgzH-14c-R1aY1VWBRSDNSMaqFd1Uz8zqXZqLwBDXuYPEEgkz0MACslsAiM1mtfm2-D7KWy-yvVP-DnMlLCYaIKUBfO7XiaQjXIlcnAtrYMXTOvC31THl6jnXVmPzWvyZkQUaZkR86SkJcBKSMDfvlHasui1oTatVrSOwWDIp0ld4RYiCx3_4EwTms-6tTFjEZDoz9juEuUTAvoZnhnmu5XEFW1SWKq1t4U3-uqBvrpsk08DbUEbnonSu59L5oCcZJegCUZj-fjEzctHbn0Ljcv86A8dPLLAKmWjMjOyXd3aInEP3Z7NBJfdQY0w5yi6gYxTyNDV_QghyBgJ2mrd_31qz5WGmZjl428PYfyoSBh4TRe8lzyGKoSGw7b8e37gDxLAG2Xu-I6wFJXOkeUFQep0=w275-h412-no",["Local Headmaster","Chair of The Parish Council","Local Vicar","Traffic Warden"],1],
 
     ["What year did the building of the chapel begin?", "https://lh3.googleusercontent.com/-mrYjmkYPFkg/VjON-nIVJxI/AAAAAAAACdU/ZBzsDjSBvnE/s400/P1240185.JPG",["1859","1869","1879"],1],
@@ -12,7 +12,7 @@ var questions = [
     ["How long is the Ardley Tunnel under these fields?","https://lh3.googleusercontent.com/-RyOgQlTspns/WJZAbJjStBI/AAAAAAAADOQ/ab9VyTQWs8QFjCOkawK4RNP6NSgUmCtYgCMQC/s400/20170204_151340.jpg",["955 yards (873m)","1,055 yards (965m)", "1,155 yards (1,056m)"],2],
 
 
-    ["What year did Wordsworth visit Souldern","https://lh3.googleusercontent.com/-54IerkhcLEk/VhgN_0F1p3I/AAAAAAAACBU/S47G0T2-IYo/s400/William_Wordsworth_001.jpg",["1810","1820","1830"],1],
+    ["What year did Wordsworth visit Souldern?","https://lh3.googleusercontent.com/-54IerkhcLEk/VhgN_0F1p3I/AAAAAAAACBU/S47G0T2-IYo/s400/William_Wordsworth_001.jpg",["1810","1820","1830"],1],
 
     ["If you leave Souldern via this tunnel, which village will you enter?","https://lh3.googleusercontent.com/-m_eNzZ5kJ3k/WRNNTaU5CTI/AAAAAAAADnA/CyeqDWa6aLMcckXYIfjVPypbadQ2SlRjgCMQC/s400/20170510_083552.jpg",["Fritwell","Clifton","Aynho"],2],
 
@@ -36,7 +36,7 @@ var questions = [
 
     ["What is the name of this house overlooking the village pond?","https://www.souldern.org/home/houses/soulderncourt.jpg",["Souldern Manor", "Souldern Court", "Souldern House","The Hermitage"],1],
 
-    ["Which of these towns is furthest away by road","https://lh3.googleusercontent.com/-CyzkAA4ePnw/Vg68Oc4gfxI/AAAAAAAABvg/Y5UITGzTqf4/s400/local-map.png",["Brackley","Bicester","Banbury"],2],
+    ["Which of these towns is furthest away by road?","https://lh3.googleusercontent.com/-CyzkAA4ePnw/Vg68Oc4gfxI/AAAAAAAABvg/Y5UITGzTqf4/s400/local-map.png",["Brackley","Bicester","Banbury"],2],
     
     
 ];
