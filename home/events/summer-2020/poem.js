f5e10e65f511ce3aa1533a79e6dbc145769c7bc8
David Carlisle <d.p.carlisle@gmail.com>
2020-08-09 00:20:24 +0100

    updates

--- home/events/summer-2020/poem.js
+++ home/events/summer-2020/poem.js
@@ -5,6 +5,7 @@ galleries.poem = {
     url: "https://photos.app.goo.gl/jgCKsv3n1kG3p4hDA",
     album: [
 
+"https://lh3.googleusercontent.com/kiy6AkjlBpmdwbcrPRmkM5xAE7MAZShN9XC48TTF4a7r4o_8hV6LV5wdy1PSzDs9FpjhU6p3K12F9G3PqN1lrCktKpD__ykF3mGXngN8LDv7TCKy7rGJQrGx1pGowQTPprC92WFpNw",
 
 ]
 };
