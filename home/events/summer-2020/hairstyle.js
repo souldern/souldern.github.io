77dcaa086ccd1479d7fbdbd54b5e0e67b21d2c18
David Carlisle <d.p.carlisle@gmail.com>
2020-08-16 15:23:23 +0100

    updates

--- home/events/summer-2020/hairstyle.js
+++ home/events/summer-2020/hairstyle.js
@@ -5,6 +5,7 @@ galleries.hairstyle = {
     url: "https://photos.app.goo.gl/pYPf2xWnBhH3C2Xf7",
     album: [
 
+"https://lh3.googleusercontent.com/j2MWgnqe3h8rCjwAH9moLGmm_lQQqhCoWTRznTa56D9DGNaeDPefFn_nf5uA9QOE7SQUBC2eANa5BXgvpNVpiW-6Rqegcfxz31iPZe_vo7qeISVLg7pFQZsHYAbhzP0Zef7EwoWamg",
 
 ]
 };
