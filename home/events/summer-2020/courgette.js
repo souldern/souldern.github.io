3fcc9147e6b32e6bb430346c9cebcf6b71fe8069
David Carlisle <d.p.carlisle@gmail.com>
2020-08-21 14:10:04 +0100

    updates

--- home/events/summer-2020/courgette.js
+++ home/events/summer-2020/courgette.js
@@ -6,7 +6,9 @@ galleries.courgette = {
     album: [
 
 "https://lh3.googleusercontent.com/21tgdLvB9Mk67vn3MW1qxpqrKmUrRV83Dod2zvtMhWE2A30EH-dqlFLRow1Hl9p6w5SiD9nF5NXiCuNAJsOIx_GUyxpwyB1SNSdM1NLFrSZmBnRIWR1TJXd84utYrqcKWuIpZLbWOQ",
-"https://lh3.googleusercontent.com/0A3o8qP_sfyBjBrqJS5ktmi1XMA5gQE_7AQrlMqmZpfBxDuwpiVVKPBgEKNnPm9CzVPim_xgvtv7ew7mSOqOZvUsg1g8rFtaV7m-tVQuqbQwSNaFdFPhyxu8nyxpVlWXNzwXNanesA",
+"https://lh3.googleusercontent.com/w9rL19P0vSFCTnQjRyn-pUoTJFymhd5xv32vEiBUvTd8YLUd9WAQnx9JfjKi6-wZfulAeUGU8ZiV55B98cHjmoIzxqA0ni2RvhC7VAh02BnfW7FPL6FkbgIForLfs3K_Tr1FT45VWA",
+"https://lh3.googleusercontent.com/r63n0sS3hSgJvhycwf8T61HaNgj5bOGnLuf5sjNmfVHO6EkviQAbDpDIQf7cslJ3rifQPU-CgmnGENoup4TxWDUo8PhBpJc1Mn2SeoGIR4YXT4Z8abysFE0qn3wmeJGOHLtwIbfgJg",
+"https://lh3.googleusercontent.com/de1vyRXK0J3fMzZsPL39HkQZ5A7i1woeB6pOo3f1n_xqoftJWp3MnwmzUeb50BWblhoEvvmIudAqBPLWFDU6UfQR3UhBRSrCpS2bZpKwsFxhdjy1QrU89gBysGnVmhCYyc2Kq97B9A",
 
 ]
 };
