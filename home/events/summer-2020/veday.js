541d81a31604876a5dcedb7833b2d94b085c0e39
David Carlisle <d.p.carlisle@gmail.com>
2020-08-22 18:21:25 +0100

    updates

--- home/events/summer-2020/veday.js
+++ home/events/summer-2020/veday.js
@@ -5,6 +5,10 @@ galleries.veday = {
     url: "https://photos.app.goo.gl/PAid97LYpC9mkb4Z8",
     album: [
 
+"https://lh3.googleusercontent.com/MoZll8GuGi-ACKGy1RiEKOQP9rW5gaoKxUYxsC9KC-mwKFk9_h-Mu2MtjyZOib89fjFhuZQkCkJ2-W8XvAUrekPbg2rQARxoBcdeiy4HCW1zh8mARCVWJQR8Mmb3e3v6ghK1jb6syw",
+"https://lh3.googleusercontent.com/0_eU_ozqklZvvveClIF928Z2XxkQOdvik9LmgTYfThd_4jBbUFBJMpAT3fLlZ92PNpmWSrwaHapY4l8ByIJ03NYEFtqw3-4DATvJjsimBFRJeL3k0UacwF4xNoUSrP4A9VEVgnR5wA",
+"https://lh3.googleusercontent.com/H0B0pu65GCa6XSikE4A5Misdfbf7KKACnrvUAj2HYx0bv8oaZYnzRhGC-VGVxRtXDUNkkqHSsTS3RtYCEP1TUnFcr1ua8Dm-w6z9qTc4luR8B7kpZrxIGk1PX7OsB-1uyBwGXrTc8w",
+"https://lh3.googleusercontent.com/KP-3pudJLm29wyLRqjhlflRNa4Sf4kFD2rxQ11dbDOebPu2HVXtaYnecF_MRGlpYZROYCbp2S3Imq_Q--y2hCQUyA3eXa_KzUYEnWcnYYHe1rOQkqCfEH0ai4CtOrnHCoNmVQh5obA",
 
 ]
 };
