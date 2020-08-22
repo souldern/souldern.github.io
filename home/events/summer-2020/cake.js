64fddd8ccdd4c93c23746fd545333c3fa3b8af84
David Carlisle <d.p.carlisle@gmail.com>
2020-08-22 17:45:22 +0100

    updates

--- home/events/summer-2020/cake.js
+++ home/events/summer-2020/cake.js
@@ -5,6 +5,7 @@ galleries.cake = {
     url: "https://photos.app.goo.gl/SQ4Nno3tmbJikcZ16",
     album: [
 
+"https://lh3.googleusercontent.com/gx7SVaRH0ABZf3reEEtzPmwAJDYsFMAr2dLbOCAkinz_ToJagIJDzl-OOH3_YMd2xbyyiLU3z0-6wEc_UPY_dTvSCEV0rLwycQd1p4Bc_0ppIgS5C_IcuO2ZEUn_SXvp45hIMJpdQQ",
 
 ]
 };
