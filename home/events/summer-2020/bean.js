dc2f532eaef77836aa82833a7684ff57f576a107
David Carlisle <d.p.carlisle@gmail.com>
2020-08-23 20:09:19 +0100

    updates

--- home/events/summer-2020/bean.js
+++ home/events/summer-2020/bean.js
@@ -5,6 +5,7 @@ galleries.bean = {
     url: "https://photos.app.goo.gl/5pZanfDN3uAvWtC27",
     album: [
 
+"https://lh3.googleusercontent.com/EGkG8uJO9yYWLw27Gl_ilURSQJBFXM5eAVIASaByvIPams7YQeb_eYvHu53PypPCHrReJsCI-MwfbgRekMVkxqGsFya2WV2bNf_9SSLIKt395D-o5WrElmTEABXDsydGABaQh4fveg",
 
 ]
 };
