2f1ce8ccb2fa30342d29038739e4d08e311f9e7f
David Carlisle <d.p.carlisle@gmail.com>
2020-08-27 14:37:09 +0100

    updates

--- home/events/summer-2020/holiday.js
+++ home/events/summer-2020/holiday.js
@@ -5,8 +5,8 @@ galleries.holiday = {
     url: "https://photos.app.goo.gl/QWgBzpQ3RUDbi6L39",
     album: [
 
-"https://lh3.googleusercontent.com/sfK212oXK62EumEyUINg6ezPjzXG__HIXR7_rd5Oz_2t9aR69jfp9X1m48Uq1sXrliJKGBxh47RsavkIYCpr7v7QGAYpZ6yf5RkeH9r-IFRpeac_B83wmawuYVzxOIztDTUpJm8Mhg",
-"https://lh3.googleusercontent.com/cFQSwyjGFQMzwIIY7GvqdX9r6-WoQRGQOvxVe5f70enDHnmMyzOO4uGzi36i1cNm68blLcxenc4lUTs0jwwtsEqZPlKQ9KmLfTADDeQ80QrIlxNvbg8z2ZPA0-ZN9kuESBl2u_OSXw",
+"https://lh3.googleusercontent.com/TWoHLQtP7ppbLoZ3cfCt1XSYd6Z0tszGijJO4m-XuLP9M5EO_3oGvwDjmN_KorBr-RikynhPPUUZagRUv7YVEROciRDV8CEUQ1_-B1lEFV91MgpajpzIvp-0mazHgoSZrhtR5g9B4A",
+"https://lh3.googleusercontent.com/i-fHJv5lvFSYYCHHsI0zBMD-3RwdK0b5hyTxnxl2jP0rMngVOY2zDYGEe4MdFZJ2nyDzyFDob4KM_u-rpyQ3FXFmBKby2huYuUQtfBqrBKrRpZSryBgDpTcwR7t7c6vrYyL4UFzP8g",
 
 ]
 };
