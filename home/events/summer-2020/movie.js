e2fb7e93f954c3c60ea0891b87d7825fb6547e6d
David Carlisle <d.p.carlisle@gmail.com>
2020-08-28 20:05:55 +0100

    updates

--- home/events/summer-2020/movie.js
+++ home/events/summer-2020/movie.js
@@ -5,6 +5,7 @@ galleries.movie = {
     url: "https://photos.app.goo.gl/Arw4UkFriEg5wiPc8",
     album: [
 
+"https://lh3.googleusercontent.com/9ljFqHHOupNjTqy9Axmm1uVRa1vIyndWqv-STayu-6CisPPs2nMoKKopLBk_lwVLSBJhP1ATRP4ElLV804o024OjULZTmbomAC8L3Jrxm6iIfF9_SRLoHuaAJdDocXjAqDh6OFaY",
 
 ]
 };
