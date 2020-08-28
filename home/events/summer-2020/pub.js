b0791f8a96e70034dc0eac7760b8b8076b9b167c
David Carlisle <d.p.carlisle@gmail.com>
2020-08-28 22:12:38 +0100

    updates

--- home/events/summer-2020/pub.js
+++ home/events/summer-2020/pub.js
@@ -5,6 +5,7 @@ galleries.pub = {
     url: "https://photos.app.goo.gl/qcqESkW977ngQ3vE7",
     album: [
 
+"https://lh3.googleusercontent.com/Y2WEhHZ47zKc0MmdJ3wcqV1cJlscmQ8fIU8OjkzxDIWgtOzJMlbU7u23eKRS44r5SH7zGXK29hD9dUaX05WUBH_Ci01GSA3BLjD8EcdUIoENuFV1n2AWtch4vFurN3XjWxVn1Jv3",
 
 ]
 };
