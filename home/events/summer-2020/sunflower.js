24ccbc0462ddb6682ed9d7d08b7bc4195cd7de14
David Carlisle <d.p.carlisle@gmail.com>
2020-08-28 16:33:56 +0100

    updates

--- home/events/summer-2020/sunflower.js
+++ home/events/summer-2020/sunflower.js
@@ -8,7 +8,7 @@ galleries.sunflower = {
 "https://lh3.googleusercontent.com/u0KVf0YP0WnO_bZLrgYyA0i1Sdmr8f8Ho8HKzNxVoDlAvLQwKiMnLQK2OrgC0cN3eSN4Y6R8KzzHE72UaLZGseua5yEIrR82LZ0IvdA9skmALu-mriX1W-0QfPioWRe0toYDZlVd4A",
 "https://lh3.googleusercontent.com/Ug1TyU9LeVv5JmeqVjgAAFgXcIfTYh_nROByC0h8OunAaCQ3RYOAxc60njTvH79wzEHZvl3De61KcIY8Y4d39wEplg3X449eEL3v-Eer65B-SUIb8uXIalZ5Y_uC_XSNfHWhb6ubFw",
 "https://lh3.googleusercontent.com/3KE68_Hp-tv3RvN4xyoQhcKq22bz1jK2ABAWr7kT8dteJhvo-fk9CrOzovckkPSBkhcywaVgasKAGknqkckfw-HqTYjcVY8Vx3dAcwQQa9UVdbcmN-1xibX9tLTw2w6r0DiRDP4Q-Q",
-"https://lh3.googleusercontent.com/BoYA3Lc0MdP6xg-YL36Pby8tTQEgKKnPF77jUIo4o_Ps-gGYHMqULhDrgWYsp0g0HWvBNY_r9MXkkgWCfowePm61Xl--4tdB4IRM_x7ONUX53c4B2OdjfTDZXbfr5eSMpaJtcTku9g",
+"https://lh3.googleusercontent.com/fbYOv9xeW47dEt5PGO7GwoZV3psH_kWzTYWI14MLszcjJYz5GbxCU2CSca7cmSepXnt8gjyv1kSPFxajRv8_ruhOpBYUjtnG_zCkbh5hJYrrG-lw83WrcZUi8-YVHU6tWJk5fsAJ4g",
 
 ]
 };
