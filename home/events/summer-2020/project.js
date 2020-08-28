416fda0fa87c5dd994f9b07dcfb9a53ea6eefabc
David Carlisle <d.p.carlisle@gmail.com>
2020-08-28 16:42:14 +0100

    updates

--- home/events/summer-2020/project.js
+++ home/events/summer-2020/project.js
@@ -6,6 +6,7 @@ galleries.project = {
     album: [
 
 "https://lh3.googleusercontent.com/-OQ93skSJnuYJoWsbbQKpiMZIUtTrgEqDyS2mHwyIiJ7nriTCGaqM6-SsOC6J9rzSxeB0U2PmP5eoblwIfjMtJkP0DXetQzNV-npUneiTWg42h4PjgD5WL_gT7wk39mXZquyDLfE8g",
+"https://lh3.googleusercontent.com/YRGnJciM67NDJ2HMJxS5xk-2xTqbQHultqBmuutb31XV1W06uyS_Xq3bNJXh4bOqFFzkLTU2xvrSvXt-uNJJOpBVhk64wR3KcdZ3nvL5O4liwnJzQu-7junZPW8x3Yvr6OFwNbMMvA",
 
 ]
 };
