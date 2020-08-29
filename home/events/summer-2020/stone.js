2f0dfe73623c32c3392031c361a46e05d57f1777
David Carlisle <d.p.carlisle@gmail.com>
2020-08-29 16:58:27 +0100

    updates

--- home/events/summer-2020/stone.js
+++ home/events/summer-2020/stone.js
@@ -9,6 +9,7 @@ galleries.stone = {
 "https://lh3.googleusercontent.com/FEUSkDOWssuRaD3czuQw8JWD2MSIxJLqWn4TtWteEJi_UCz4VSHeWXQEneR0bCADDlLEZmmIcwXvdYFAHx6uc20wHe47ud0xTU4zjCKkxE3GPCx6zGxGskqYivYzsI94KZhQefS4_g",
 "https://lh3.googleusercontent.com/p0SsCCyN4PdVLhv-NF5uk4wRHiPc5lVzTehTHzcjuptA8gMYbMbr5c-1jKOhDXhzKS0UMBxBpg17BX2pHQPn9r3eyTEzE_IZUUrFUydananesqEyJQtf0BTGqaWEgRN0OLscBRZBjA",
 "https://lh3.googleusercontent.com/-2g9hocRaCgH2XfFpwIhqQLjifnqe-vVazBofPwKSp4LsfByHTQJ42YD_4OuaqmXezDSoOXgIXSeuSJvrkvX21yCIZgzFI8A5IQFssNs8SaA1v7ZW1zkLRAVdxHA2iflMCJPrvMFI38",
+"https://lh3.googleusercontent.com/k4JBwZKzIh8vIEcdNE0bMqNr3tGbAp6WlyLBSZKZiq8JvoH62UF4yFzpF_Ty5UD9KrIexU7cPwCV05MDJydQB7-XLvrUyF9xboI7rRwzE_s_7ijEjOh-9HEtEmMcUOtEO8M6pVrM4w",
 
 ]
 };
