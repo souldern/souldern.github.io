dbf036ac31b4416ccb7fba0982572a7039c4fbd8
David Carlisle <d.p.carlisle@gmail.com>
2020-08-29 11:06:59 +0100

    updates

--- home/events/summer-2020/allotment.js
+++ home/events/summer-2020/allotment.js
@@ -6,6 +6,7 @@ galleries.allotment = {
     album: [
 
 "https://lh3.googleusercontent.com/t50SKHjzxh1v56kw4UirxWLCUItq9lkqXn9zAgUVcOUOuIj60Ya49Jf7ZPThhoqrd4oAI9c9v64y2rATH8rgA1gLDs1WSj3vmB94iuLaOH_Z9nNqj-jjF5edY6YySWx7tpU12-fnag",
+"https://lh3.googleusercontent.com/6NKvY5GbFPoE3-7o5UtwncfZnKXjdeoDJuEZN-09o099Q3SjFXChHqKecR-vzdSSJsd7u26BpKS6YcmoVcsVrprQG2b71NgyHLycjkAZdfYct5mKUMtzfEWjyAe2KBrucNtu7_EE",
 
 ]
 };
--- home/events/summer-2020/bean.js
+++ home/events/summer-2020/bean.js
@@ -9,6 +9,7 @@ galleries.bean = {
 "https://lh3.googleusercontent.com/rdKGgTWBmQc__AmIfEw_sjGw4vXMhDI1vK6fOPGqtjmPh9xks8opsF6W3CNZG5-PVlorQTtIxDUGQonKid97e98Ef9TNOBjrvhMG0f-1afuccl63XbzTPa90b4vw7uDnIQ-qxe2lOg",
 "https://lh3.googleusercontent.com/saFv2YNuhsX9nI95ffCxw3KX82F8QqmUTGJk-kJ6oJWtJxpkgaK3VkckLYha5j0S2JR_MNzVghvNktltEHAE4Y-HI2_lhHb9Rq4TVF4BIM_KgWyGCHfKLeyCJGUb8FNeM3vpzOLKlA",
 "https://lh3.googleusercontent.com/R5cGCb00e6G1q5VfyZQ2z_qyLAowpqZOgCfyjhFbeFQDwTbo9B_YrW2L9ls6tujadfakolw9C2YEPK1K7FKg2do5h0MKN7lDHAx8Roqaw0UsaErKyP1OfaZIZnYuPe5PPjHBA7I7pA",
+"https://lh3.googleusercontent.com/htTcWMUOK2Xuhpsl_M07ybh8KoJ-iviCZ7PHYGv-PayMxj55q9Nk2oL-3DY-VC7GeXBJm2lj8nl3mXOFyX9zj8EZMOp2Dq7vlSmCFVf72LFTNgyJVYKlBmYYvgzGVrWRtwvC6nEqOA",
 
 ]
 };
