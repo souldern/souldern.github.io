42e61e3674fb49b92d3e12f154e2d391e742520a
David Carlisle <d.p.carlisle@gmail.com>
2020-08-29 16:19:59 +0100

    updates

--- home/events/summer-2020/doortrait.js
+++ home/events/summer-2020/doortrait.js
@@ -5,6 +5,7 @@ galleries.doortrait = {
     url: "https://photos.app.goo.gl/FHrKctR5iaVKk4cN6",
     album: [
 
+"https://lh3.googleusercontent.com/UTkZrjuzAsYeoxLGNVFYfJdPTjEhR4i7QIgwpkafrv9cCNNLFXk_Hp_tcDcGGrISktpZBa7-3azOkB1Y63ydQaRiT3uL5aknDk21yeQaGCe7ob5uimnGjvpwO7lKR4RI2CKBTuvSlw",
 
 ]
 };
