6704ed35abd47f365002b2a61727f7ddfe3fc3d3
David Carlisle <d.p.carlisle@gmail.com>
2020-08-29 15:50:17 +0100

    updates

--- home/events/summer-2020/fairy.js
+++ home/events/summer-2020/fairy.js
@@ -6,6 +6,7 @@ galleries.fairy = {
     album: [
 
 "https://lh3.googleusercontent.com/2s43GikHoKzP62SDmkdHAx5I3ok-8yDGzxYmObLg0ioG2ptlDXRS7CO7z2SBHt62Qbo5ruoa74iGX2118bwEgV2YWrLfPACo_Kjf965M5Gu0WM-XWO-ZGttLmyVVtJ2BIDKgkovIDA",
+"https://lh3.googleusercontent.com/4-jINOGKo3pIB4h_5s3zhyr1YmtkfO9PuEgPzXzT_st6a3rGBvIH9EnfMfFnYVON3Lq_OdYlbUKrgCCvcIdp9a-KWhmdyq-Cu5CXHf5usZgMwRtSbkf7sgvpQgqB7Bt2ZyEN_ze5",
 
 ]
 };
