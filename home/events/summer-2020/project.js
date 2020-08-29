92898b3ea29f82bbf53104f09dc334dfc53f9dcd
David Carlisle <d.p.carlisle@gmail.com>
2020-08-29 11:56:55 +0100

    updates

--- home/events/summer-2020/project.js
+++ home/events/summer-2020/project.js
@@ -5,6 +5,7 @@ galleries.project = {
     url: "https://photos.app.goo.gl/MwAgaHN7xUUr2HF7A",
     album: [
 
+"https://lh3.googleusercontent.com/iJlMyD1nUFjK9eTGR84jdj1rp1IcLjIaD7ozu41LuZPMARWt1RHzJsLfuNlwEBeqIJe9kriIAemI73f3yC8WGPgajFeBKESWD0-UPBdXMPU2ZCUG6-IMCujghkt3c9yY0PAEDgQzS4U",
 "https://lh3.googleusercontent.com/-OQ93skSJnuYJoWsbbQKpiMZIUtTrgEqDyS2mHwyIiJ7nriTCGaqM6-SsOC6J9rzSxeB0U2PmP5eoblwIfjMtJkP0DXetQzNV-npUneiTWg42h4PjgD5WL_gT7wk39mXZquyDLfE8g",
 "https://lh3.googleusercontent.com/OyX5e7ohmqBCBGeILh7izJSD3oSm023VFxDOEd_JMzKY8_VCQgWJWrm2bcj4TSPARpJIw--LImGn2XBYJHMc9TwtjMa2kERb0JXppbqbxwgp9uFbW7QmcLBQvVGlJQ0vW8-2Vb9wfw",
 "https://lh3.googleusercontent.com/Z7kRzftaRotOtX-zT2r1FLFu531UpL5IL95kPlv2X-SOlhM-oXEKx0sRjv02jDA-MlNbDOwyxEmwa40v-XQxnbEeN4a4CcGocjGmMQE83mQdF2hADTtqoVo7hvZpYS136inHFwQnLA",
