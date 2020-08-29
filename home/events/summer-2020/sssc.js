41e7c9df123d23142bfa865edaef5e6f14ccd9bb
David Carlisle <d.p.carlisle@gmail.com>
2020-08-29 19:40:04 +0100

    updates

--- home/events/summer-2020/sssc.js
+++ home/events/summer-2020/sssc.js
@@ -5,6 +5,7 @@ galleries.sssc = {
     url: "https://photos.app.goo.gl/M79Hch9v3ErAHdRB9",
     album: [
 
+"https://lh3.googleusercontent.com/yRu1rJmPRNteoJZrH4yIQSEyzJLeKWCCi6up2d5LfJBjBEHIN0Dst4YGuTZu_kZqpKwAIryfbWS0Aqd4k6siw20lNHxaEx8MssNK8UwFNWR6PwP_Oj45ihf000OSepVqQwVNbn5cNA",
 
 ]
 };
