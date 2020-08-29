0a6ae6918a4e9b87dadc8191dc2dfe8bd221deed
David Carlisle <d.p.carlisle@gmail.com>
2020-08-30 00:14:46 +0100

    updates

--- home/events/summer-2020/pub.js
+++ home/events/summer-2020/pub.js
@@ -6,7 +6,7 @@ galleries.pub = {
     album: [
 
 "https://lh3.googleusercontent.com/Y2WEhHZ47zKc0MmdJ3wcqV1cJlscmQ8fIU8OjkzxDIWgtOzJMlbU7u23eKRS44r5SH7zGXK29hD9dUaX05WUBH_Ci01GSA3BLjD8EcdUIoENuFV1n2AWtch4vFurN3XjWxVn1Jv3",
-"https://lh3.googleusercontent.com/AD0EMPtdN9Ggh06tkIBtp8CNuGSHwx_N1KKfZKovu-Z-1_qH505b4_nBsoeZtXL7RP1Uzh0ue4GatCjltyavtiJWvYc6d34az0rlVhJUBXDudkDPp6bvOEparZ4GVyC1Lhv9trGms9U",
+"https://lh3.googleusercontent.com/6_Ll8mDFcvImdvd-6s-3Wrz2EKrSE-tjcUPzeiSe4616vKzwRfUioGdIpYa3TmiLCJy9Xfbn9kQ-vokWdWyzKQu6_xg_wLi1iVVCnnk3ZIAw1ILhA_uG1E-Qn__m_vY-Nk9uadK_vLw",
 
 ]
 };
