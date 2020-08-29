409822aee12b84acc4a4b091763a8f73cec05037
David Carlisle <d.p.carlisle@gmail.com>
2020-08-30 00:05:25 +0100

    updates

--- home/events/summer-2020/pub.js
+++ home/events/summer-2020/pub.js
@@ -6,6 +6,7 @@ galleries.pub = {
     album: [
 
 "https://lh3.googleusercontent.com/Y2WEhHZ47zKc0MmdJ3wcqV1cJlscmQ8fIU8OjkzxDIWgtOzJMlbU7u23eKRS44r5SH7zGXK29hD9dUaX05WUBH_Ci01GSA3BLjD8EcdUIoENuFV1n2AWtch4vFurN3XjWxVn1Jv3",
+"https://lh3.googleusercontent.com/AD0EMPtdN9Ggh06tkIBtp8CNuGSHwx_N1KKfZKovu-Z-1_qH505b4_nBsoeZtXL7RP1Uzh0ue4GatCjltyavtiJWvYc6d34az0rlVhJUBXDudkDPp6bvOEparZ4GVyC1Lhv9trGms9U",
 
 ]
 };
