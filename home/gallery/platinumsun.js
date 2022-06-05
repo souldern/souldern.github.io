f89d9629d65eee90ea55153549c47f113a694bc0
David Carlisle <d.p.carlisle@gmail.com>
2022-06-05 16:56:47 +0100

    updates

--- home/gallery/platinumsun.js
+++ home/gallery/platinumsun.js
@@ -5,6 +5,8 @@ galleries.platinumsun = {
     url: "https://photos.app.goo.gl/GCJqf1DMDdrxr93t7",
     album: [
 
+"https://lh3.googleusercontent.com/elW9-b5zs1uFbMOIhhlMM6zol2WtOWs08q1r1Tl0QtzZpiVg3wtbdLTOLFnIRszPZkP-Q1untLDnbKdIRabjmopejIHsjv5GAyv7DzzxRrkWq6Br1XMGGLUpmLsbFroVu5-fx95q7w",
+"https://lh3.googleusercontent.com/elW9-b5zs1uFbMOIhhlMM6zol2WtOWs08q1r1Tl0QtzZpiVg3wtbdLTOLFnIRszPZkP-Q1untLDnbKdIRabjmopejIHsjv5GAyv7DzzxRrkWq6Br1XMGGLUpmLsbFroVu5-fx95q7w",
 
 ]
 };
