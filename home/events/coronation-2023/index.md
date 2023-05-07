40e8c731d7d7de27f3dd2e03e5ad6408c727b0e9
David Carlisle <d.p.carlisle@gmail.com>
2023-05-07 23:47:32 +0100

    updates

---
+++ home/events/coronation-2023/index.md
@@ -0,0 +1,25 @@
+---
+layout: default
+title: Coronation Big Lunch
+---
+<style>
+#poster img {margin:0 auto;display:block; height:12em;}
+</style>
+
+
+# Souldern Coronation Big Lunch
+
+
+
+<div id="poster" markdown="1">
+
+![poster](big-lunch-2023.jpg)
+</div>
+
+
+<div id="biglunch2023"></div>
+
+
+<script src="/home/gallery/biglunch2023.js"></script>
+<script src="/home/gallery/galleries.js"></script>
+
---
+++ home/gallery/biglunch2023.js
@@ -0,0 +1,18 @@
+var galleries = galleries || {};
+
+galleries.biglunch2023 = {
+    title: "Coronation Big Lunch",
+    url: "https://photos.app.goo.gl/iWBEY2EXn1VfUG2Y7",
+    album: [
+
+"https://lh3.googleusercontent.com/pw/AJFCJaUb_cjQA0Qo1yACRECsTo0q6y6wMXAvrLmjHOLgt68bt52qlXVIEjg1DIlvxScAYVZUlQb5ld56ikZGv92R2JEMdikWxC3JUpCpmxkh-8HuCZ89_ms",
+"https://lh3.googleusercontent.com/pw/AJFCJaWgebJRqcmQf70lzwwcJNfl__HQqke-ZqTPWmlO04SSwK1RUit_KxUHRn9XHT0jS00Ri6_nv_0JMzVzH0s_NN6nA4IHtsoC61iVEkrl4TD60ij9l2c",
+"https://lh3.googleusercontent.com/pw/AJFCJaUxS9y_L5E-ac6GcmLFP3M-kgKD49JCYudtPZa2m5w3qnnjtAifynq739aipoxVKOIrnmxvC3yIO35eY_yW-RY9lZvzmcpDrdx0XrclycG0dkHpPYI",
+"https://lh3.googleusercontent.com/pw/AJFCJaV8WboWtSZK95GH_tmxFd97X_dFoEpv8bXLa7ax-ZCFJNRJlEUn8ly1lHd-KMdk3N76ZeZDVx5v7i1hG2k7bHKq0Eggicb6goULoRibXnYRfgs8VUY",
+"https://lh3.googleusercontent.com/pw/AJFCJaUd9FlOCNV6MkWZURQPUWODH0gBQr0AbxzdUqrOVjDDbGUJkIeiu-IzpsjGqMYLTSYyz6Tf14gtwJEFzxAY9q_r5tjmt6QPs_Yfpp7mWdix26Q4084",
+"https://lh3.googleusercontent.com/pw/AJFCJaVjcGKagyYKPP5rc8OmnXaFBQAc1C3baqaJEwgDjT_XD9mb-NEN-iR06F0LJT1jHOjz2842lE-zaFv12RZNZ9UUoJ0u4bzD1o4sLovFFyt5xhUaYZo",
+"https://lh3.googleusercontent.com/pw/AJFCJaV9E8oYQjJ3gT4hKmoFu3mfdGJBBnfKySCXhqyHuwsXfG-eXPpGGF8U1yPhMWTrZuTvb8Ft2xodlpX-PJ0UdQGQJo855bzWCY6zpWLJqSpJce22Ye4",
+"https://lh3.googleusercontent.com/pw/AJFCJaVJgu9j-QZa5XDsmBo6nM9JHxDl7eEruwwvmXf3W1wIlqtaltmfyfQQEZzW6pV4aoLncXe8NWf80tHf29iMoYkgNYtW9ZaULUWag-I--yg8fLkFVqM",
+
+]
+};
