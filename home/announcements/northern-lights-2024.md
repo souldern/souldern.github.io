d5b14843ec0be1a4079b61bd2cbfed48401dc420
David Carlisle <d.p.carlisle@gmail.com>
2024-05-11 09:13:55 +0100

    updates

---
+++ home/announcements/northern-lights-2024.md
@@ -0,0 +1,18 @@
+---
+date: 2024-05-11
+title: Northern Lights
+---
+
+# Northern Lights over Souldern
+
+
+
+<div id="nlights2024"></div>
+<script src="/home/gallery/nlights2024.js"></script>
+<script src="/home/gallery/galleries.js"></script>
+<script>
+  document.getElementById("nlights2024").style.height="30em";
+</script>
+
+
+The Northern lights over Souldern, May 10th 2024.
---
+++ home/gallery/nlights2024.js
@@ -0,0 +1,20 @@
+var galleries = galleries || {};
+
+galleries.nlights2024 = {
+    title: "Northern Lights May 2024",
+    url: "https://photos.app.goo.gl/XsbwvNqZkVRRMHD39",
+    album: [
+
+"https://lh3.googleusercontent.com/pw/AP1GczNuWppYmy8feRUnocGbUUVj4DMC4k-IoDQ4_6hCqC994hDOUKujAINoDosy_aklRZ3Z_osHENp9j3JPrtCt0wSxQiBWX5jMJKklX5gB7YepzQyE8hM",
+"https://lh3.googleusercontent.com/pw/AP1GczMwV34sAitQrH-Gtmg6Bo_rK-2Ng5UpqKk9y48JuJ7DrzuVJwzs2BUlRYzHFCB-cYX0WgudZRnALpt4_i5uKzLJYJ0rPbOkaw5g2iYKHhZUlSC0U0E",
+"https://lh3.googleusercontent.com/pw/AP1GczMu54jQxI-E48FPoXtX3OmWWNNxDipZwf8kWFFo3peyW1HBtwd8SLAMaxGXFoedV48UvA8BaY2d4jhC5eWB-BoTETfabEauX7-OAvIz5XE4uG5JpU4",
+"https://lh3.googleusercontent.com/pw/AP1GczO9XgPkm3uKyok2XXed3vVGlZn2dXGYhj-36PW-oAuXX7KkZwAZt6S0Dn94lHL2mV8f8JnrPFV5ARkCdboxYMXBAII3ipl9e9S0WsspFGFuXXupNGI",
+"https://lh3.googleusercontent.com/pw/AP1GczN0k2i58KPkIr97LR1rLNLH7iJYc7XbfhNpwgwXIrkhgRQAuzZ9XDkxR80MC1CzfrNuWyfFmn2b-u0QnAjUbGlbU67o7AzZ-KA7p32NPLU8AzipSbo",
+"https://lh3.googleusercontent.com/pw/AP1GczMJaBfhhOqifMm70FjaGMEqiOz3JK6vwKeswFDtIgC7OkohLkZji6vl5N-yq3JXrSyqWgoTJPnLV4WuoyuTKk_oZ01ftVxTHKgbABU3vQBf9vOoHso",
+"https://lh3.googleusercontent.com/pw/AP1GczMqfcfV_o3nhHM9NYOhU9Fsn_Ire4zhHKjBWn4O2krT3AFf3g7LwJ164HByKUnuJkj6WTXLhX6kB-XLKxNS_hHq84VVh6he9axE_1-Et5UnIbEN1yM",
+"https://lh3.googleusercontent.com/pw/AP1GczPlkF5exypd7DyGnMQRiTlzja-2SBlUTdPJ7YPLMZCtg6WSXFZCPFzLnRTkz5-Mmx2P-ZGJ_K1B5uP4-9tBf2M2VEpu-5DgIc8itEI_KIa2V3mymQs",
+"https://lh3.googleusercontent.com/pw/AP1GczPoUmoFWzmxbHqAUW5qp-bfB2cSANasvTDjpUly5yBL9FzmoMKAvUT31HUHTiX7vOoQZIAOrGatNh9ECBmaDCylyPT4tpk_yefv8IYe7ytul-TJqP0",
+"https://lh3.googleusercontent.com/pw/AP1GczPY_zVVPJhCed5FEh7hnfhv9E8r45I2bxkwm4jYbBilfd7fNZApIYHxqOWuEAyWbYoaAf8TBq0TVygzyIrphYYGmNNj3RpTG41k0vkn321OGvQskd4",
+
+]
+};
